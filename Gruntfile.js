c94c819e6feb8976db473aafa6600dbfd45fc95f
Jag Reehal <jag.reehal@gmail.com>
2014-02-01 15:54:29 +0000

    Log subhead and line

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,11 @@
+/*global module:false*/
+
 module.exports = function(grunt){
+	"use strict";
+
+	grunt.registerTask('log', function(){
+		grunt.log.subhead('A bold head');
+		grunt.log.writeln('A new line');
+	});
 
 };
